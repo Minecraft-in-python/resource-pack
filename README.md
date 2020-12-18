3d59cc076cedb43ce7d3a4624ce4827debf9d3d6
jason-bowen-zheng <18917572736@wo.cn>
2020-12-18 12:09:50 +0000

    2020-12-18

---
+++ README.md
@@ -0,0 +1,10 @@
+# 资源包
+这是 Minecraft-in-python 的资源包, 安装:
+```shell
+# Github 服务器
+git clone https://github.com/Minecraft-in-python/resource-pack
+# Gitee 服务器
+git clone https://gitee.com/Minecraft-in-python/resource-pack
+cd resource-pack
+./install.py
+```
---
+++ install.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from os import environ, mkdir, path
+from shutil import copytree, rmtree
+from sys import platform
+
+def install():
+    print('copy resource pack... ', end='')
+    MCPYPATH = search_mcpy()
+    if not path.isdir(path.join(MCPYPATH, 'resource-pack')):
+        mkdir(path.join(MCPYPATH, 'resource-pack'))
+    if path.isdir(path.join(MCPYPATH, 'resource-pack', 'default')):
+        rmtree(path.join(MCPYPATH, 'resource-pack', 'default'))
+    copytree(get_dir('pack'), path.join(MCPYPATH, 'resource-pack', 'default'))
+    print('done')
+
+def get_dir(d):
+    return path.abspath(path.join(path.dirname(__file__), d))
+
+def search_mcpy():
+    # 搜索文件存储位置
+    if 'MCPYPATH' in environ:
+        MCPYPATH = environ['MCPYPATH']
+    elif platform.startswith('win'):
+        MCPYPATH = path.join(path.expanduser('~'), 'mcpy')
+    else:
+        MCPYPATH = path.join(path.expanduser('~'), '.mcpy')
+    return MCPYPATH
+
+if __name__ == '__main__':
+    install()
